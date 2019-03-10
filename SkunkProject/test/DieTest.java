89084712890dec1cf5440dc71c38fd0e86d11226
Anna <anna.koehler@stthomas.edu>
2019-03-10 12:22:32 -0500

    loaded die test

--- SkunkProject/test/DieTest.java
+++ SkunkProject/test/DieTest.java
@@ -1,5 +1,8 @@
 import static org.junit.Assert.*;
 
+import java.util.Arrays;
+
+import org.junit.Assert;
 import org.junit.Test;
 
 public class DieTest {
@@ -7,6 +10,21 @@ public class DieTest {
 	@Test
 	public void test() {
 		fail("Not yet implemented");
+	}
+	
+	@Test
+	public void loadedDieTest() {
+		int[] progRoll = {1,2,6,5};
+		Die die1 = new Die (progRoll);
+		int[] dieTest = new int[10];
+		for (int i=0; i<=3; i++) {
+			die1.roll();
+			dieTest[i] = die1.getLastRoll();
+			System.out.println(dieTest[i]);
+		}
+		Assert.assertEquals(
+				Arrays.toString(dieTest), 
+				Arrays.toString(new int[] {1,2,6,5}));
 	}
 
 }
