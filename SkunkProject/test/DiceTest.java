00af04bf5d28395eb27950b4b4737f689a09c5ec
Chint <Chint@Chint-PC>
2019-03-10 14:26:21 -0500

    8th commit - Updated DiceTest.java

--- SkunkProject/test/DiceTest.java
+++ SkunkProject/test/DiceTest.java
@@ -1,12 +1,57 @@
-import static org.junit.Assert.*;
-
+import static org.junit.Assert.assertEquals;
+import org.junit.Before;
 import org.junit.Test;
 
-public class DiceTest {
+public class DiceTest
+{
+	private Dice dice;
+	private Die die1, die2;
+	private int[] rollDie1, rollDie2;
 
-	@Test
-	public void test() {
-		fail("Not yet implemented");
+	@Before
+	public void setUp() throws Exception
+	{
+ 		this.rollDie1 = new int[] { 1, 2, 3 };
+		this.die1 = new Die(rollDie1);
+
+		this.rollDie2 = new int[] { 1, 2, 3 };
+		this.die2 = new Die(rollDie2);
+		
+		this.dice = new Dice(die1, die2);
 	}
 
+	@Test
+	public void test_Init_PredictableDie()
+	{
+		dice.roll();
+		int value = dice.getLastRoll();
+		assertEquals(4, value);
+	}
+
+	@Test
+	public void test_Predictable_Roll_2()
+	{
+		dice.roll();
+		dice.roll();
+		int value = dice.getLastRoll();
+		assertEquals(6, value);
+	}
+
+	@Test
+	public void test_Predictable_Roll_3()
+	{
+		dice.roll();
+		dice.roll();
+		dice.roll();
+		int value = dice.getLastRoll();
+		assertEquals(2, value);
+	}
+
+	@Test
+	public void test_toString()
+	{
+		dice.toString();
+		assertEquals("Dice with last roll:   =>  + ", "Dice with last roll:   =>  + ");
+	}
+	
 }
--- SkunkProject/test/DieTest.java
+++ SkunkProject/test/DieTest.java
@@ -7,24 +7,25 @@ import org.junit.Test;
 
 public class DieTest {
 
-	@Test
-	public void test() {
-		fail("Not yet implemented");
-	}
 	
 	@Test
 	public void loadedDieTest() {
 		int[] progRoll = {1,2,6,5};
 		Die die1 = new Die (progRoll);
-		int[] dieTest = new int[10];
-		for (int i=0; i<=3; i++) {
+		int[] dieTest = new int[4];
+
+		
+		for (int arrayIndex=progRoll.length-1; arrayIndex>=1; arrayIndex=arrayIndex -1) {
 			die1.roll();
-			dieTest[i] = die1.getLastRoll();
-			System.out.println(dieTest[i]);
+			dieTest[arrayIndex] = die1.getLastRoll();
+			System.out.println(dieTest[arrayIndex]);
 		}
-		Assert.assertEquals(
-				Arrays.toString(dieTest), 
-				Arrays.toString(new int[] {1,2,6,5}));
+			
+		
+		String actual = Arrays.toString(dieTest);
+		String expected = Arrays.toString(new int[] {1,2,6,5});
+		
+		Assert.assertEquals(actual,expected);
 	}
 
 }
