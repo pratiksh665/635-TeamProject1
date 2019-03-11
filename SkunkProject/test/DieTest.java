01138f60700c42a41867a9166d6a8b7724c680bd
Anna <anna.koehler@stthomas.edu>
2019-03-10 21:14:26 -0500

    jUnit tests - passed!

--- SkunkProject/test/DieTest.java
+++ SkunkProject/test/DieTest.java
@@ -3,28 +3,39 @@ import static org.junit.Assert.*;
 import java.util.Arrays;
 
 import org.junit.Assert;
+import org.junit.Rule;
 import org.junit.Test;
+import org.junit.rules.ExpectedException;
 
 public class DieTest {
-
-	@Test
-	public void test() {
-		fail("Not yet implemented");
-	}
+	@Rule
+	  public final ExpectedException exception = ExpectedException.none();
 	
 	@Test
+	//Test that preprogrammed Die object is working as expected
 	public void loadedDieTest() {
 		int[] progRoll = {1,2,6,5};
 		Die die1 = new Die (progRoll);
 		int[] dieTest = new int[10];
-		for (int i=0; i<=3; i++) {
+		//roll preprogrammed Die object 10 times; should run through the preprogrammed values then start over
+		for (int i=0; i<=9; i++) {
 			die1.roll();
 			dieTest[i] = die1.getLastRoll();
 			System.out.println(dieTest[i]);
 		}
 		Assert.assertEquals(
-				Arrays.toString(dieTest), 
-				Arrays.toString(new int[] {1,2,6,5}));
+				Arrays.toString(new int[] {1,2,6,5,1,2,6,5,1,2}), 
+				Arrays.toString(dieTest));
 	}
-
+	
+	@Test
+	//test that the nullPointerException is thrown when an empty array is passed as a parameter to Die obj
+	public void nullExceptionTest() {
+		exception.expect(NullPointerException.class);
+		exception.expectMessage("Empty Array Initialized");
+		int[] emptyArray = null;
+		Die empty = new Die(emptyArray);
+	}
+	
+	
 }
